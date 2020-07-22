f87d95eecc2c65141dfbe89f095ba65f531f672f
Rajan Gurung <rajangrg1@hotmail.com>
2020-07-22 20:48:50 +1200

    service API adjusted to accept user input

--- src/Services/jobAPI.js
+++ src/Services/jobAPI.js
@@ -1,16 +1,20 @@
 import axios from "axios";
 
-export const getAllJobApi = async () => {
+const BASE_URL = `https://thingproxy.freeboard.io/fetch/https://jobs.github.com/positions.json`;
+
+export const getAllJobApi = async (jobTiltle, location) => {
+  let changableUrl = BASE_URL;
+  //validation
+  if ( jobTiltle && location) {
+    changableUrl = BASE_URL + `?description=${jobTiltle}&location=${location}`;
+  }
+
   const response = await axios
-    .get(
-      `https://thingproxy.freeboard.io/fetch/https://jobs.github.com/positions.json`
-    )
+    .get(changableUrl)
     .then(({ data }) => data)
     .catch((err) => console.error(err));
-    return response;
+  return response;
 };
-
-
 
 //for future reference
 //https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?description=python&location=new+york
